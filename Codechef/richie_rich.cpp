db6d91e5f44ef2547511d58129eeaef3ed409918
munibashami07 <munibashami07@gmail.com>
2026-08-05 07:30:32 +0000

    Aug 5, 2026, 12:30 AM

--- Codechef/richie_rich.cpp
+++ Codechef/richie_rich.cpp
@@ -1,3 +1,33 @@
+// Richie Rich
+// Chef aims to be the richest person in Chefland by his new restaurant franchise. Currently, his assets are worth 
+// A
+// A billion dollars and have no liabilities. He aims to increase his assets by 
+// X
+// X billion dollars per year.
+
+// Also, all the richest people in Chefland are not planning to grow and maintain their current worth.
+
+// To be the richest person in Chefland, he needs to be worth at least 
+// B
+// B billion dollars. How many years will it take Chef to reach his goal if his value increases by 
+// X
+// X billion dollars each year?
+
+// Input
+// The first line contains an integer 
+// T
+// T, the number of test cases. Then the test cases follow.
+// Each test case contains a single line of input, three integers 
+// A
+// A, 
+// B
+// B, 
+// X
+// X.
+// Output
+// For each test case, output in a single line the answer to the problem
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
